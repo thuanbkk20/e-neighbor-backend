8b4fbf4ad4094698e94487104918f1c8a38eef0a
Minh Thuan <thuan17082002@gmail.com>
2023-11-07 09:16:05 +0700

    Update user profile

--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -1,10 +1,17 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserRepository } from '../repositories/user.repository';
 import { UserNotFoundException } from 'src/exceptions';
 import { UserEntity } from '../domains/entities/user.entity';
 import { RegisterDto } from 'src/modules/auth/domains/dtos/sign-in.dto';
 import { generateHash } from '@/common/utils';
 import { GoogleSignInDto } from './../../auth/domains/dtos/google-sign-in.dto';
+import { UserUpdateDto } from '../domains/dtos/user-update.dto';
+import { validateHash } from './../../../common/utils';
+import { UserDto } from '../domains/dtos/user.dto';
 
 @Injectable()
 export class UserService {
@@ -60,5 +67,22 @@ export class UserService {
 
   async findByRequiredInfo(findOptions: object): Promise<UserEntity | null> {
     return await this.userRepository.findOneBy(findOptions);
+  }
+
+  async updateUser(user: UserUpdateDto): Promise<UserDto> {
+    const userInDB = await this.findOneById(user.id);
+    console.log(userInDB);
+    const isPasswordMatched = await validateHash(
+      user.password,
+      userInDB.password,
+    );
+    console.log(isPasswordMatched);
+    if (!isPasswordMatched) {
+      throw new UnauthorizedException();
+    }
+    user.password = generateHash(user.password);
+    const { password, ...returnUser } =
+      await this.userRepository.updateUser(user);
+    return returnUser;
   }
 }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { UserService } from './services/user.service';
 import { UserRepository } from './repositories/user.repository';
 import { UserController } from './controllers/user.controller';
+import { AdminService } from '@modules/admin/services/admin.service';
+import { AdminModule } from '@modules/admin/admin.module';
+import { LessorModule } from '@modules/lessor/lessor.module';
 
 @Module({
+  imports: [AdminModule, LessorModule],
   exports: [UserService],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, AdminService],
   controllers: [UserController],
 })
 export class UserModule {}
