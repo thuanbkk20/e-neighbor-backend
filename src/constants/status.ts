dde4634d67b393c7d55154cc098869dc4af596ed
Minh Thuan <thuan17082002@gmail.com>
2023-11-23 21:32:01 +0700

    Create product entity

---
+++ src/constants/status.ts
@@ -0,0 +1,7 @@
+import { type ValueOf } from './../interfaces';
+export enum STATUS {
+  AVAILABLE = 'available',
+  NOT_AVAILABLE = 'not_available',
+}
+
+export type StatusType = ValueOf<typeof STATUS>;
