af27cf1f7756d73d6c5dd531ec83437fc2ce203f
Minh Thuan <thuan17082002@gmail.com>
2023-11-23 12:52:01 +0700

    Add PaymentMethodEntity

---
+++ src/constants/payment-method.ts
@@ -0,0 +1,7 @@
+import { type ValueOf } from '../interfaces';
+export const PAYMENT_METHOD = {
+  BANKING: 'BANKING',
+  E_WALLET: 'E WALLET',
+} as const;
+
+export type PaymentMethodType = ValueOf<typeof PAYMENT_METHOD>;
---
+++ src/modules/payment/domains/entities/payment.entity.ts
@@ -0,0 +1,23 @@
+import {
+  PAYMENT_METHOD,
+  PaymentMethodType,
+} from './../../../../constants/payment-method';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { AbstractEntity } from '../../../../common/abstract.entity';
+import { UserEntity } from '../../../user/domains/entities/user.entity';
+
+@Entity('payment_method')
+export class PaymentMethodEntity extends AbstractEntity {
+  @Column()
+  name: string;
+
+  @Column({ enum: PAYMENT_METHOD })
+  type: PaymentMethodType;
+
+  @Column()
+  accountNumber: number;
+
+  @ManyToOne(() => UserEntity, (user) => user.id)
+  @JoinColumn()
+  user: UserEntity;
+}
