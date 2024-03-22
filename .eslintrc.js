3abe5f795d9bdf1e164389b1baa0cca568fa93b6
Nice <hoangkimc93@gmail.com>
2024-03-22 19:59:01 +0700

    refactor(code-convention): add husky and lodash

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js, commitlint.config.js'],
 
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
@@ -66,6 +66,7 @@ module.exports = {
       },
     ],
   },
+  ignorePatterns: ['*.config.js', '*.js'],
   settings: {
     'import/resolver': {
       node: {
