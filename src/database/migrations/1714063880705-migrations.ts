8fd9477fa2fc227c9a93cce0c1f7744cadf76253
Minh Thuan <thuan17082002@gmail.com>
2024-04-25 23:56:39 +0700

    Update migration - may need to remove all rental fee and order rows before migration

--- src/database/migrations/1714063880705-migrations.ts
+++ src/database/migrations/1714063880705-migrations.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Migrations1714060765298 implements MigrationInterface {
-  name = 'Migrations1714060765298';
+export class Migrations1714063880705 implements MigrationInterface {
+  name = 'Migrations1714063880705';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "thirdparty-payment" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "merchant_id" character varying NOT NULL, "merchant_name" character varying NOT NULL, "amount" integer NOT NULL, "message" character varying NOT NULL, "bank" character varying NOT NULL, "transaction_id" character varying NOT NULL, "order_id" integer, CONSTRAINT "REL_a17591606406fe1156a01ae86f" UNIQUE ("order_id"), CONSTRAINT "PK_828462544059c20e8b4edb70d53" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "thirdparty-payment" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "merchant_id" integer NOT NULL, "merchant_name" character varying NOT NULL, "amount" integer NOT NULL, "message" character varying NOT NULL, "bank" character varying NOT NULL, "transaction_id" character varying NOT NULL, "order_id" integer, CONSTRAINT "REL_a17591606406fe1156a01ae86f" UNIQUE ("order_id"), CONSTRAINT "PK_828462544059c20e8b4edb70d53" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `ALTER TABLE "orders" DROP COLUMN "delivery_address"`,
